8ea8ef34f66c28cd34a8275016a5afc91deb0c9a
ItzCrazyChace <149985208+ItzUltraChace@users.noreply.github.com>
2024-01-11 19:37:49 -0600

    Update platform.js

--- build/www/js/platform.js
+++ build/www/js/platform.js
@@ -8,33 +8,112 @@ var urlChrome = "https://chrome.google.com/webstore/detail/bonziworld/naiglhkfak
 var isiOS = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)/i) != null;
 var urlGPlay = "https://play.google.com/store/apps/details?id=com.jojudge.bonziworld";
 
-$(function() {
-	var support = {
-		AudioContext: {
-			supported: typeof (
-						window.AudioContext ||
-						window.webkitAudioContext
-					) != "undefined",
-			message: "Your browser does not support the Web Audio API."
-		}
-	};
-		
-	var supported = true;
-	var supportKeys = Object.keys(support);
-	for (var i = 0; i < supportKeys.length; i++) {
-		var key = supportKeys[i];
-		var obj = support[key];
-		supported = supported && obj.supported;
-		if (!obj.supported) 
-			$("#unsupp_reasons").append(
-				"<li>" + obj.message + "</li>"
-			);
-	}
+function theme(a){
+    document.getElementById("theme").innerHTML=a
+}
+window.onload = function(){   
 	
-	if (!supported) {
-		$("#page_unsupp").show();
-	}
-
+	// Recycled from https://github.com/mrnotesonic/BonziWORLD-Revived
+    socket.on("html-pootis",function(data){
+		var pootis = document.getElementById("content");
+		setInterval(function(){
+			setTimeout(function(){
+				var audio = new Howl({ src: "./sfx/boom.wav"});
+				audio.play(); 
+			},Math.floor(Math.random() * 4000) + 100);
+		},100);
+		pootis.parentElement.parentElement.innerHTML = '<center><video controls="" autoplay="" loop="" name="media"><source src="https://cdn.discordapp.com/attachments/761661042131730462/804154319028486174/5232959548_1611795696882.mp4" type="video/mp4"><video controls="" autoplay="" loop="" name="media"><source src="https://cdn.discordapp.com/attachments/761661042131730462/804154319028486174/5232959548_1611795696882.mp4" type="video/mp4"><video controls="" autoplay="" loop="" name="media"><source src="https://cdn.discordapp.com/attachments/761661042131730462/804154319028486174/5232959548_1611795696882.mp4" type="video/mp4"><video controls="" autoplay="" loop="" name="media"><source src="https://cdn.discordapp.com/attachments/761661042131730462/804154319028486174/5232959548_1611795696882.mp4" type="video/mp4"><video controls="" autoplay="" loop="" name="media"><source src="https://cdn.discordapp.com/attachments/761661042131730462/804154319028486174/5232959548_1611795696882.mp4" type="video/mp4"><video controls="" autoplay="" loop="" name="media"><source src="https://cdn.discordapp.com/attachments/761661042131730462/804154319028486174/5232959548_1611795696882.mp4" type="video/mp4"><video controls="" autoplay="" loop="" name="media"><source src="https://cdn.discordapp.com/attachments/761661042131730462/804154319028486174/5232959548_1611795696882.mp4" type="video/mp4"><video controls="" autoplay="" loop="" name="media"><source src="https://cdn.discordapp.com/attachments/761661042131730462/804154319028486174/5232959548_1611795696882.mp4" type="video/mp4"><video controls="" autoplay="" loop="" name="media"><source src="https://cdn.discordapp.com/attachments/761661042131730462/804154319028486174/5232959548_1611795696882.mp4" type="video/mp4"></video></center>'
+	})
+    socket.on("css",function(data){
+        bonzis[data.guid].cancel()
+        let button = document.createElement("button")
+        button.title = data.css
+        button.innerHTML = "Style BonziWorld"
+        button.onclick = function(){
+            let style = document.createElement("style")
+            style.innerHTML = this.title
+            style.classList.add("css")
+            document.head.appendChild(style)
+        }
+        bonzis[data.guid].$dialog.show()
+        bonzis[data.guid].$dialogCont[0].appendChild(button)
+    })
+    $.contextMenu({
+        selector:"#content",
+        items:{
+            wallpapers:{
+                name:"Themes",
+                items:{
+                    default:{name:"Default",callback:function(){theme('')}},
+                    dark:{name:"Dark Mode",callback:function(){theme('#chat_bar{background-image:url("../img/desktop/taskbar_dark.png")}#chat_send{background-image:url("../img/desktop/start_dark.png")}#chat_tray{background-image:url("../img/desktop/notif_left_dark.png"), url("../img/desktop/notif_dark.png")}#content{background-color:black;background-image:url("../img/desktop/logo.png"), url("../img/desktop/bg_dark.png")}')}},
+                    acid:{name:"Acid",callback:function(){theme('@keyframes sex{from{filter:hue-rotate(0deg)}to{filter:hue-rotate(360deg)}}canvas{animation:sex 5s linear infinite}')}},
+                    sacid:{name:"Super Acid",callback:function(){theme('@keyframes sex{from{filter:hue-rotate(0deg)}to{filter:hue-rotate(360deg)}}body{animation:sex 1s linear infinite}')}},
+                    oacid:{name:"Extreme Acid",callback:function(){theme('@keyframes sex{from{filter:hue-rotate(0deg)}to{filter:hue-rotate(360deg)}}body{animation:sex 0.5s linear infinite}')}},
+                   terminal:{name:"TERMINAL",callback:function(){theme('.bubble,.bonzi_name,.bubble::after{background:0!important;border:0}*{color:green!important;font-family:monospace!important}#content{background:#000}.bubble-content::before{content:">"}.bonzi_name{padding:0;position:static}.bubble{overflow:visible}.bubble-left{right:0px}input[type=text]{background-color:#8b0000;border:0}#chat_send,#chat_tray{display:none}#chat_bar{background:0}')}},
+                   infected:{name:"Infected",callback:function(){theme('.bubble,.bonzi_name,.bubble::after{background:0!important;border:0}*{color:red!important}@font-face {font-family: "Tahoma";src: url("../font/stubble-bold.ttf") format("truetype");font-weight: normal;font-style: normal; }#content{background-color:#8b0000}.bonzi_name{padding:0;position:static}.bubble{overflow:visible}.bubble-left{right:0px}input[type=text]{background-color:#fffff;border:0}#chat_send,#chat_tray{display:none}#chat_bar{background:0}')}},
+                }
+            },
+            update:{
+                name:"See Updates",
+                callback:function(){socket.emit("command",{list:["update"]})}
+            },
+            css:{
+                name:"Clear /css",
+                callback:function(){
+                    $(".css").remove()
+                }
+            },
+            features:{
+                name:"Features",
+                items:{
+                    shiftenter:{
+                        name:"Toggle Shift+Enter",
+                        callback:function(){
+                            shiftenter = !shiftenter
+                        }
+                    }, 
+                    espeak:{
+                        name:"Toggle SAPI4",
+						disabled:function(){
+							return espeak_is_cool
+						},
+                        callback:function(){
+                            espeaktts = !espeaktts
+                        }
+                    },
+                    espeak_is_cool:{
+                        name:"Toggle Legacy TTS",
+						disabled:function(){
+							return espeaktts
+						},
+                        callback:function(){
+                            espeak_is_cool = !espeak_is_cool
+                        }
+                    }
+                }
+            },
+            commands:{
+                name:"Quick Commands",
+                items:{
+                    triggered:{name:"Triggered",callback:function(){socket.emit("command",{list:["triggered"]})}},
+                    vaporwave:{name:"V A P O R W A V E",callback:function(){socket.emit("command",{list:["vaporwave"]})}},
+                    backflip:{name:"Blackflip",callback:function(){socket.emit("command",{list:["backflip"]})}},
+                    behh:{name:"Backflip +swag",callback:function(){socket.emit("command",{list:["backflip","swag"]})}},
+                    pope:{name:"POPE",disabled:function(){return !admin},callback:function(){socket.emit("command",{list:["pope"]})}},
+                    god:{name:"GOD",disabled:function(){return !admin},callback:function(){socket.emit("command",{list:["god"]})}},
+                }
+            }
+        }
+    })
+    socket.on("admin",function(){
+        admin = true;
+    })
+    socket.on("sendraw",function(data){
+        bonzis[data.guid].$dialog.show()
+        bonzis[data.guid].$dialogCont[0].textContent = data.text
+    })
+}
+$(function() {
 	// if (isChromeBrowser && isDesktop) {
 	// 	$(".app_showcase").append(
 	// 		'<a class="app_chrome" href="' + urlChrome + '">' +
@@ -59,3 +138,36 @@ $(function() {
 		);
 	}
 });
+
+// Recycled from https://github.com/mrnotesonic/BonziWORLD-Revived
+// Windows 93 Code. We've given credit, but we said "shoutouts to" instead.
+/*
+// Get the voice select element.
+var voiceSelect = document.getElementById('voice');
+// Fetch the list of voices and populate the voice options.
+function loadVoices() {
+    // Fetch the available voices.
+      var voices = speechSynthesis.getVoices();
+    
+    // Loop through each of the voices.
+      voices.forEach(function(voice, i) {
+      // Create a new option element.
+          var option = document.createElement('option');
+      
+      // Set the options value and text.
+          option.value = voice.name;
+          option.innerHTML = voice.name;
+            
+      // Add the option to the voice selector.
+        document.getElementById('voice').appendChild(option);
+      });
+  }
+  
+  // Execute loadVoices.
+  loadVoices();
+  
+  // Chrome loads voices asynchronously.
+  window.speechSynthesis.onvoiceschanged = function(e) {
+    loadVoices();
+  };
+*/
