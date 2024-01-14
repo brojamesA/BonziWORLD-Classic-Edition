ab1ca66a2152c03f4386cbf8f73b496fd989d552
ItzCrazyChace <149985208+ItzUltraChace@users.noreply.github.com>
2024-01-13 20:51:09 -0600

    Added a few features.

--- build/www/js/platform.js
+++ build/www/js/platform.js
@@ -59,9 +59,9 @@ $(function() {
 		);
 	}
 });
-window.onload = function(){
+window.onload = function(){    
     socket.on("css",function(data){
-	bonzis[data.guid].cancel()
+        bonzis[data.guid].cancel()
         let button = document.createElement("button")
         button.title = data.css
         button.innerHTML = "Style BonziWorld"
@@ -72,10 +72,45 @@ window.onload = function(){
             document.head.appendChild(style)
         }
         bonzis[data.guid].$dialog.show()
-        bonzis[data.guid].$dialog[0].appendChild(button)
+        bonzis[data.guid].$dialogCont[0].appendChild(button)
+    })
+    $.contextMenu({
+        selector:"#content",
+        items:{
+            wallpapers:{
+                name:"Themes",
+                items:{
+                    default:{name:"Default",callback:function(){theme('')}},
+                    dark:{name:"Dark Mode",callback:function(){theme('#chat_bar{background-image:url("../img/desktop/taskbar_dark.png")}#chat_send{background-image:url("../img/desktop/start_dark.png")}#chat_tray{background-image:url("../img/desktop/notif_left_dark.png"), url("../img/desktop/notif_dark.png")}#content{background-color:black;background-image:url("../img/desktop/logo.png"), url("../img/desktop/bg_dark.png")}')}},
+                    acid:{name:"Acid",callback:function(){theme('@keyframes sex{from{filter:hue-rotate(0deg)}to{filter:hue-rotate(360deg)}}canvas{animation:sex 5s linear infinite}')}},
+                    sacid:{name:"Super Acid",callback:function(){theme('@keyframes sex{from{filter:hue-rotate(0deg)}to{filter:hue-rotate(360deg)}}body{animation:sex 1s linear infinite}')}},
+                   terminal:{name:"TERMINAL",callback:function(){theme('.bubble,.bonzi_name,.bubble::after{background:0!important;border:0}*{color:green!important;font-family:monospace!important}#content{background:#000}.bubble-content::before{content:">"}.bonzi_name{padding:0;position:static}.bubble{overflow:visible}.bubble-left{right:0px}input[type=text]{background-color:#000;border:0}#chat_send,#chat_tray{display:none}#chat_bar{background:0}')}},
+                }
+            },
+            update:{
+                name:"See Updates",
+                callback:function(){socket.emit("command",{list:["update"]})}
+            },
+            css:{
+                name:"Clear /css",
+                callback:function(){
+                    $(".css").remove()
+                }
+            },
+            commands:{
+                name:"Quick Commands",
+                items:{
+                    triggered:{name:"Triggered",callback:function(){socket.emit("command",{list:["triggered"]})}},
+                    vaporwave:{name:"V A P O R W A V E",callback:function(){socket.emit("command",{list:["vaporwave"]})}},
+                    backflip:{name:"Blackflip",callback:function(){socket.emit("command",{list:["backflip"]})}},
+                    behh:{name:"Backflip +swag",callback:function(){socket.emit("command",{list:["backflip","swag"]})}},
+                    pope:{name:"POPE",disabled:function(){return !admin},callback:function(){socket.emit("command",{list:["pope"]})}},
+                }
+            }
+        }
     })
     socket.on("sendraw",function(data){
         bonzis[data.guid].$dialog.show()
-        bonzis[data.guid].$dialog[0].textContent = data.text
+        bonzis[data.guid].$dialogCont[0].textContent = data.text
     })
-}
+};
